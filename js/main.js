082eae217f2a7d2548dc9f5ec92808f86cb95e0c
Georg Felgitsch <georg.felgitsch@gmail.com>
2014-08-19 21:49:08 +0200

    minor

--- js/main.js
+++ js/main.js
@@ -314,7 +314,7 @@ function loadFile() {
 			if (u === 0) {
 
 				$("#movieCarousel").append("<li data-target=\"#myCarousel\" data-slide-to=\"" + u + "\" class=\"active\"></li>");
-				$("#carouselItems").append("<div class=\"item active\"><div class=\"fill myCarouselContent\" style=\"background-color:#CCCCCC;\"><div class=\"carousel-caption\">" + innerContent + "<br><br><img id=\"\" class=\"logos\" src=\"../images/kiZ_logo.png\"></img> <img id=\"\" class=\"logos\" src=\"../images/evolaris_logo.gif\"></img></div></div></div>");
+				$("#carouselItems").append("<div class=\"item active\"><div class=\"fill myCarouselContent\" style=\"background-color:#CCCCCC;\"><div class=\"carousel-caption\">" + innerContent + "<br><br><div style=\"color:#CCCCCC\"><img id=\"\" class=\"logos\" src=\"../images/kiZ_logo.png\"></img>as<img id=\"\" class=\"logos\" src=\"../images/evolaris_logo.gif\"></img></div></div></div></div>");
 				$("#movieCarousel").append("<li data-target=\"#myCarousel\" data-slide-to=\"14\" class=\"active\"></li>");
 				//$("#carouselItems").append(staticContent);
 				//writeLog(staticContent);
@@ -322,7 +322,7 @@ function loadFile() {
 			} else {
 
 				$("#movieCarousel").append("<li data-target=\"#myCarousel\" data-slide-to=\"" + u + "\"></li>");
-				$("#carouselItems").append("<div class=\"item\"><div class=\"fill myCarouselContent\" style=\"background-color:#CCCCCC;\"><div class=\"carousel-caption\">" + innerContent + "<br><br><div style=\"color:#CCCCCC\"><img id=\"\" class=\"logos\" src=\"../images/kiZ_logo.png\"></img>asdfasdfasdfaasdfasdfasdfasdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf <img id=\"\" class=\"logos\" src=\"../images/evolaris_logo.gif\"></img></div></div></div></div>");
+				$("#carouselItems").append("<div class=\"item\"><div class=\"fill myCarouselContent\" style=\"background-color:#CCCCCC;\"><div class=\"carousel-caption\">" + innerContent + "<br><br><div style=\"color:#CCCCCC\"><img id=\"\" class=\"logos\" src=\"../images/kiZ_logo.png\"></img>as<img id=\"\" class=\"logos\" src=\"../images/evolaris_logo.gif\"></img></div></div></div></div>");
 				$("#movieCarousel").append("<li data-target=\"#myCarousel\" data-slide-to=\"14\" class=\"active\"></li>");
 				//$("#carouselItems").append(staticContent);
 				
